ff61cf7024beeaeb2a8b9a51361576efcf8ae4b2
Pavan.N <pavan.n33@gmail.com>
2022-06-23 15:50:15 +0530

    Updated technical expertise
    Updated technical expertise

--- about-me/src/app/app.component.ts
+++ about-me/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'about-me';
+  hideHeader: boolean = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(){
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.hideHeader = event.url === "/home"
+      });
+  }
 }
+
